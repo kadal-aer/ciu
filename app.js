0c5ed5118ebdf99ee513b83b7db3de9876e64d6e
cuma-kadal <jebeairin@proton.me>
2024-06-15 01:33:32 +0700

    master

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const bytes = require('bytes')
 const logger = require('morgan')
 const express = require('express')
 const axios = require('axios')
+const caliph = require("caliph-api");
 
 const app = express()
 app.set('json spaces', 4)
 app.use(logger('dev'))
 
+const endpoint = {
+    characterai: [
+    search: 'chara/search',
+    info: 'chara/info',
+    chat: 'chara/chat'
+    ],
+    tools: [
+    ip2website: 'api/ip2website',
+    whois: 'api/whois',
+    subfinder: 'api/subfinder'
+    ]
+}
 app.all('/', (req, res) => {
 	const obj = {}
 	const used = process.memoryUsage()
@@ -21,11 +34,12 @@ app.all('/', (req, res) => {
 		success: true,
 		message: 'Hello World!',
 		uptime: new Date(process.uptime() * 1000).toUTCString().split(' ')[4],
-		status: obj
+		status: obj,
+		endpoint: endpoint
 	})
 })
 
-app.get('/api', async (req, res) => {
+app.get('/chara/chat', async (req, res) => {
     const { text, characterId } = req.query
 if (!text) {
     res.send({
@@ -91,6 +105,72 @@ app.get('/chara/search', async (req, res) => {
     axios.get(`https://andreans-cai.hf.space/api/chara/search?name=${name}`).then(({data}) => {
     return res.send({
         status: 200,
+		creator: 'RynXD',
+		result: data.result
+		});
+    }).catch((error) => {
+      console.log(error);
+      res.json(error_message);
+    });
+    }
+})
+
+app.get('/api/ip2website', async (req, res) => {
+    const { ip } = req.query
+	if (!ip) {
+    res.send({
+        status: 400,
+		creator: 'RynXD',
+		message: `ip is required`
+		});
+		}
+	if (ip) {
+    axios.get(`https://webresolver.nl/api.php?key=C24VD-5YH3C-G3G48-EILO5&json&action=ip2websites&string=${ip}`).then(({data}) => {
+    return res.send({
+		creator: 'RynXD',
+		result: data
+		});
+    }).catch((error) => {
+      console.log(error);
+      res.json(error_message);
+    });
+    }
+})
+
+app.get('/api/subfinder', async (req, res) => {
+    const { domain } = req.query
+	if (!domain) {
+    res.send({
+        status: 400,
+		creator: 'RynXD',
+		message: `domain is required`
+		});
+		}
+	if (domain) {
+    caliph.tools.subfinder(domain).then( data => {
+    return res.send({
+		creator: 'RynXD',
+		result: data
+		});
+    }).catch((error) => {
+      console.log(error);
+      res.json(error_message);
+    });
+    }
+})
+
+app.get('/api/whois', async (req, res) => {
+    const { domain } = req.query
+	if (!domain) {
+    res.send({
+        status: 400,
+		creator: 'RynXD',
+		message: `domain is required`
+		});
+		}
+	if (domain) {
+    axios.get(`https://api.botcahx.eu.org/api/webzone/whois?query=${domain}&apikey=afYO6dXO`).then(({data}) => {
+    return res.send({
 		creator: 'RynXD',
 		result: data.result
 		});
