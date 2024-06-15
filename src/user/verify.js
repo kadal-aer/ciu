18cbcae6f36900b959fe59a1016ea3fea1ba3403
cuma-kadal <jebeairin@proton.me>
2024-06-15 13:00:17 +0700

    master

--- src/user/verify.js
+++ src/user/verify.js
@@ -7,6 +7,11 @@ const waktu = new Date((new Date).toLocaleString("en-US", {
 const app = express()
 app.set('json spaces', 4)
 
+const error_message = {
+    status: 500,
+    creator: 'RynXD',
+    message: 'Internal Server Error'
+};
 async function getData() {
 const result = await axios.get('https://api.rynxd.link/api/get-data?apikey=kontol')
     return result.data
@@ -14,51 +19,78 @@ const result = await axios.get('https://api.rynxd.link/api/get-data?apikey=konto
 
 async function getUserByCode(res, code) {
     const data = await getData()
-    const userCode = []
+    const userWithCode = []
     for (const key in data.users) {
+        /*if (!data.users[key].code) {
+            res.json({
+            status: 404,
+            response: 'No user found'
+        })
+        }*/
         if (data.users[key].code === code) {
-            userCode.push(key)
+            userWithCode.push(key)
         }
     }
-    if (userCode.length < 1) {
-        return res.json({
-        status:404,
+    if (userWithCode.length < 1) {
+        res.json({
+        status: 404,
         response: 'No user found'
         })
     }
-    return userCode
+    return userWithCode
 }
 
-async function preData(data) {
+async function preData(userWithCode, ipUser) {
 const dtb = { data :{ users: {} } }
-dtb.data.users[data[0]] = {
+dtb.data.users[userWithCode[0]] = {
 register: true,
 limit: 100,
-//ip: ipUser
+ip: ipUser
 }
 return dtb
 }
 
-async function sendData() {
-const db = await preData()
+async function sendData(res, data) {
 const response = await fetch('https://api.rynxd.link/api/update-data?apikey=kontol', {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
         },
-        body: JSON.stringify(db.data)
+        body: JSON.stringify(data.data)
         })
+        if (response.ok) {
+        res.json({
+        status: 'success',
+        code: code,
+        waktu: waktu,
+        response: 'Sekarang anda telah terverifikasi, mohon gunakan bot dengan bijak'
+        })
+        } else {
+        console.log(e);
+        res.json(error_message);
+        }
 }
 
-app.get('/', (req, res) => {
+app.get('/verify', async (req, res) => {
     const ipUser = requestIP.getClientIp(req);
     const code = req.query.code
+    if (!code) {
     res.json({
-    status: 'success',
-    ip: ipUser,
-    code: code,
-    waktu: waktu
-    })
+        status: 400,
+        response: 'code is required!'
+        })
+    }
+    if (code) {
+    try {
+    const userWithCode = await getUserByCode(res, code)
+    const data = await preData(userWithCode, ipUser)
+    sendData(res, data)
+    } catch(e) {
+    console.log(e);
+    res.json(error_message);
+    }
+    }
+    
 })
 
 module.exports = app
